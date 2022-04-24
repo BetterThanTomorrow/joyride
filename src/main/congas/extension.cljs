10d96ab705443d879554b96773f49a513d0bf79d
Michiel Borkent <michielborkent@gmail.com>
2022-04-24 12:40:57 +0200

    all works

--- src/main/congas/extension.cljs
+++ src/main/congas/extension.cljs
@@ -9,19 +9,12 @@
 (defn register-disposable [^js context ^js disposable]
   (js/console.log "BOOM!!!!!!")
   (js/console.log "BOOM ctx" (some? context) "disp" (some? disposable))
-  (.push context.subscriptions disposable))
-
-(defn- register-command [command]
-  (vscode/commands.registerCommand (-> command meta :command) #(command)))
-
-(defn- setup-command [^js context command]
-  (->> (register-command command)
-       (register-disposable context)))
+  (.push (.-subscriptions context) disposable))
 
 (def ctx (sci/init {:classes {'js goog/global
                               :allow :all}}))
 
-(defn ^{:command "congas.runScript"} run-script [& script]
+(defn run-script [& script]
   (println "BOOM!")
   (let [ws-folder ^js (first js/vscode.workspace.workspaceFolders)
         ws-root ws-folder.uri.fsPath]
@@ -29,10 +22,20 @@
                       "(js/vscode.window.showInformationMessage \"Hello from SCI!!!!!!\")"
                       #_(fs/readFileSync (path/resolve ws-root ".congas/scripts/hello.cljs"))))
   )
+
+(defn- register-command []
+  (vscode/commands.registerCommand "congas.runScript" run-script))
+
+(defn- setup-command [^js context]
+  (->> (register-command)
+       (register-disposable context)))
+
+
+
 ; /Users/pez/Desktop/empty/congas/scripts/hello.cljs
 (defn ^:export activate [^js context]
   (aset js/globalThis "vscode" vscode)
-  (setup-command context #'run-script))
+  (setup-command context))
 
 (defn ^:export deactivate []
   )
