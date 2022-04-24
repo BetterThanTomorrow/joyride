31bf0cd64ddb8fdd941d4e8e8c49287f83ad7c91
Michiel Borkent <michielborkent@gmail.com>
2022-04-24 15:03:39 +0200

    promesa support

--- src/main/congas/extension.cljs
+++ src/main/congas/extension.cljs
@@ -3,7 +3,8 @@
    ["path" :as path]
    ["vscode" :as vscode]
    [promesa.core :as p]
-   [sci.core :as sci]))
+   [sci.core :as sci]
+   [sci-configs.funcool.promesa :as pconfig]))
 
 (defn register-disposable [^js context ^js disposable]
   (js/console.log "BOOM!!!!!!")
@@ -11,16 +12,21 @@
   (.push (.-subscriptions context) disposable))
 
 (def ctx (sci/init {:classes {'js goog/global
-                              :allow :all}}))
+                              :allow :all}
+                    :namespaces (:namespaces pconfig/config)}))
 
 (defn run-script [& script]
   (println "BOOM!")
   (let [ws-folder ^js (first js/vscode.workspace.workspaceFolders)
-        ws-root ws-folder.uri.fsPath]
+        ws-root (some-> ws-folder (.uri) (.fsPath))]
     (sci/eval-string* ctx
                       "(js/vscode.window.showInformationMessage \"Hello from SCI!!!!!!\")"
-                      #_(fs/readFileSync (path/resolve ws-root ".congas/scripts/hello.cljs"))))
-  )
+                      #_(fs/readFileSync (path/resolve ws-root ".congas/scripts/hello.cljs")))
+    (sci/eval-form ctx
+                   '(do (require '[promesa.core :as p])
+                        (p/do
+                          (p/delay 2000)
+                          (js/vscode.window.showInformationMessage "Hello from SCI again!!!!!!"))))))
 
 (defn- register-command []
   (vscode/commands.registerCommand "congas.runScript" run-script))
