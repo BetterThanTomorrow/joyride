649d3cf236022780bbe76a1569af699cf9cc130a
Michiel Borkent <michielborkent@gmail.com>
2022-04-24 20:50:33 +0200

    yo

--- src/main/congas/extension.cljs
+++ src/main/congas/extension.cljs
@@ -1,28 +1,55 @@
 (ns congas.extension
   (:require
    ["vscode" :as vscode]
-   [goog.object :as gobj]
+   [clojure.string :as str]
    [sci-configs.funcool.promesa :as pconfig]
-   [sci.core :as sci]))
+   [sci.core :as sci]
+   [promesa.core :as p]))
 
 (defn register-disposable [^js context ^js disposable]
   (.push (.-subscriptions context) disposable))
 
-(def ctx (sci/init {:classes {'js goog/global
-                              :allow :all}
-                    :namespaces (:namespaces pconfig/config)}))
+(defn debug [& xs]
+  (apply vscode/window.showInformationMessage (into-array (mapv str xs))))
+
+(def !ctx (volatile!
+           (sci/init {:classes {'js goog/global
+                                :allow :all}
+                      :namespaces (:namespaces pconfig/config)
+                      :load-fn (fn [{:keys [namespace opts]}]
+                                 (when ;; assume npm library
+                                     (string? namespace)
+                                   (if (= "vscode" namespace)
+                                     (do (sci/add-class! @!ctx 'vscode vscode)
+                                         (sci/add-import! @!ctx (symbol (str @sci/ns)) 'vscode (:as opts))
+                                         {:handled true})
+                                     (let [mod (js/require namespace)
+                                           ns-sym (symbol namespace)]
+                                       (sci/add-class! @!ctx ns-sym mod)
+                                       (sci/add-import! @!ctx (symbol (str @sci/ns)) ns-sym
+                                                        (or (:as opts)
+                                                            ns-sym))
+                                       {:handled true}))))})))
+
+(defn eval-query []
+  (p/let [input (vscode/window.showInputBox #js {:placeHolder "(require '[\"path\" :as path]) (path/resolve \".\")"
+                                                 :prompt "Type one or more expressions to evaluate"})
+          res (sci/eval-string* @!ctx input)]
+    (vscode/window.showInformationMessage (str "The result: " res))))
 
 (defn run-script [& script]
-  (let [ws-folder ^js (first js/vscode.workspace.workspaceFolders)
-        ws-root (some-> ws-folder (.uri) (.fsPath))]
-    (sci/eval-string* ctx
-                      "(js/vscode.window.showInformationMessage \"Hello from SCI\")"
-                      #_(fs/readFileSync (path/resolve ws-root ".congas/scripts/hello.cljs")))
-    (sci/eval-form ctx
-                   '(do (require '[promesa.core :as p])
-                        (p/do
-                          (p/delay 2000)
-                          (js/vscode.window.showInformationMessage "Hello from SCI again!!!!!!"))))))
+  (let [program (str/join "\n"
+                          (map pr-str '[(require '["vscode" :as vscode])
+                                        (vscode/window.showInformationMessage "Hello from SCI!")]))]
+    (sci/eval-string* @!ctx
+                      program
+                      #_(fs/readFileSync (path/resolve ws-root ".congas/scripts/hello.cljs"))))
+  (sci/eval-form @!ctx
+                 '(do (require '[promesa.core :as p])
+                      (p/do
+                        (p/delay 2000)
+                        (vscode/window.showInformationMessage "Hello from SCI again!!!!!!"))))
+  (eval-query))
 
 (defn- register-command []
   (vscode/commands.registerCommand "congas.runScript" run-script))
@@ -33,9 +60,8 @@
 
 
 
-; /Users/pez/Desktop/empty/congas/scripts/hello.cljs
+                                        ; /Users/pez/Desktop/empty/congas/scripts/hello.cljs
 (defn ^:export activate [^js context]
-  (gobj/set js/globalThis "vscode" vscode)
   (setup-command context))
 
 (defn ^:export deactivate []
