bd18050b6d969cf6888d62183e38e30287cb3fd0
Peter Strömberg <pez@pezius.com>
2022-05-01 14:45:55 +0200

    Make nrepl load-file set  `joyride.core*file*`

--- examples/.joyride/scripts/ignore_form.cljs
+++ examples/.joyride/scripts/ignore_form.cljs
@@ -1,8 +1,17 @@
 (ns ignore-form
   (:require ["vscode" :as vsode]
             [promesa.core :as p]
-            [joyride.core :as j]
             [z-joylib.editor-utils :as eu]))
+
+(def f *file*)
+(defonce run-main? true)
+
+(comment
+  ;; Loading this in the REPL w/o evaluating `main`:
+  (defonce run-main? false)  ; <- First evaluate this
+  (ns-unmap *ns* 'run-main?) ; <- Evaluate this when you are done
+                             ;    or want to test-run the script
+  )
 
 (defn main []
   (p/let [editor ^js vscode/window.activeTextEditor
@@ -12,9 +21,8 @@
     (aset editor "selection" original-selection)
     (p/do! (eu/insert-text!+ "#_" editor insert-position))))
 
-(when j/*file*
+(when run-main?
   (main))
 
-(comment  
-  (main)
-  )
+(comment
+  (main))
