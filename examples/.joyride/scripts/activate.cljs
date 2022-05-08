3ea57b1d36f6ea41e64a8a2122f8e4e623fda3d7
Peter Strömberg <pez@pezius.com>
2022-05-08 14:58:26 +0200

    Add workspace activate example

---
+++ examples/.joyride/scripts/activate.cljs
@@ -0,0 +1,6 @@
+(ns activate
+  (:require [joyride.core :as joyride]
+            ["vscode" :as vscode]
+            [promesa.core :as p]))
+
+(println "Hello World, from Workspace activate.cljs script")
