ed72ffe0ebdd6d81c919239aa69c9aa0633addb7
Peter Strömberg <pez@pezius.com>
2022-05-14 00:32:09 +0200

    Add menu items to navigate between menus

--- src/main/joyride/scripts_handler.cljs
+++ src/main/joyride/scripts_handler.cljs
@@ -2,6 +2,7 @@
   (:require ["path" :as path]
             ["vscode" :as vscode]
             [joyride.config :as conf]
+            [joyride.constants :as const]
             [joyride.db :as db]
             [joyride.sci :as jsci]
             [joyride.utils :as utils :refer [cljify jsify]]
@@ -50,7 +51,9 @@
 (defn- show-script-picker'+
   [title file-infos more-menu-items]
   (p/let [file-items (into [] (file-info->menu-item file-infos))
-          menu-items (conj file-items more-menu-items)
+          menu-items (into file-items 
+                           (into [{:kind vscode/QuickPickItemKind.Separator}] 
+                                 more-menu-items))
           script-info (vscode/window.showQuickPick (jsify menu-items) #js {:title title})]
     (cljify script-info)))
 
@@ -66,15 +69,15 @@
               picked-script (show-script-picker'+ title file-infos more-menu-items)]
         picked-script)))
 
-(defn handle-script-menu-selection+ 
+(defn handle-script-menu-selection+
   [{:keys [title] :as menu-conf} script-fn+ base-path scripts-path]
   (p/let [pick (show-script-picker+ menu-conf base-path scripts-path)]
-     (when pick
-       (let [relative-path (:relative-path pick)
-             function (:function pick)]
-         (cond
-           relative-path (script-fn+ title base-path scripts-path relative-path)
-           function (function))))))
+    (when pick
+      (let [relative-path (:relative-path pick)
+            function (:function pick)]
+        (cond
+          relative-path (script-fn+ title base-path scripts-path relative-path)
+          function (function))))))
 
 (defn run-script+
   ([menu-conf base-path scripts-path]
@@ -117,15 +120,45 @@
    conf/user-config-path
    conf/user-scripts-path])
 
+(defn menu-label-with-icon [label icon]
+  (str "$(" icon ")" (:space const/glyph-chars) label))
+
+(defn function-menu-item [{:keys [label function description icon]}]
+  {:label (menu-label-with-icon label icon)
+   :description description
+   :function function})
+
 (declare open-user-script+)
+(declare run-user-script+)
 (declare open-workspace-script+)
+(declare run-workspace-script+)
+
+(def open-workspace-script-menu-item (function-menu-item
+                                      {:label "Open Workspace Script..."
+                                       :icon "go-to-file"
+                                       :function open-workspace-script+}))
+
+(def open-user-script-menu-item (function-menu-item
+                                 {:label "Open User Script..."
+                                  :icon "go-to-file"
+                                  :function open-user-script+}))
+
+(def run-workspace-script-menu-item (function-menu-item
+                                     {:label "Run Workspace Script..."
+                                      :icon "play"
+                                      :function run-workspace-script+}))
+
+(def run-user-script-menu-item (function-menu-item
+                                {:label "Run User Script..."
+                                 :icon "play"
+                                 :function run-user-script+}))
 
 (defn run-workspace-script+
   ([]
    (apply run-script+ (run-or-open-workspace-script-args
                        {:title "Run Workspace Script"
-                        :more-menu-items {:label "Open Workspace Script"
-                                          :function open-workspace-script+}})))
+                        :more-menu-items [open-workspace-script-menu-item
+                                          run-user-script-menu-item]})))
   ([script]
    (apply run-script+ (conj (run-or-open-workspace-script-args "Run") script))))
 
@@ -133,17 +166,17 @@
   ([]
    (apply run-script+ (run-or-open-user-script-args
                        {:title "Run User Script"
-                         :more-menu-items {:label "Open User Script"
-                                           :function open-user-script+}})))
+                        :more-menu-items [open-user-script-menu-item
+                                          run-workspace-script-menu-item]})))
   ([script]
    (apply run-script+ (conj (run-or-open-user-script-args "Run") script))))
 
 (defn open-workspace-script+
   ([]
-   (apply open-script+ (run-or-open-workspace-script-args 
+   (apply open-script+ (run-or-open-workspace-script-args
                         {:title "Open Workspace Script"
-                         :more-menu-items {:label "Run Workspace Script"
-                                           :function run-workspace-script+}})))
+                         :more-menu-items [run-workspace-script-menu-item
+                                           open-user-script-menu-item]})))
   ([script]
    (apply open-script+ (conj (run-or-open-workspace-script-args "Open") script))))
 
@@ -151,7 +184,7 @@
   ([]
    (apply open-script+ (run-or-open-user-script-args
                         {:title "Open User Script"
-                         :more-menu-items {:label "Run User Script"
-                                           :function run-user-script+}})))
+                         :more-menu-items [run-user-script-menu-item
+                                           open-workspace-script-menu-item]})))
   ([script]
    (apply open-script+ (conj (run-or-open-user-script-args "Open") script))))
