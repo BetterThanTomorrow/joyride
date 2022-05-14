b14fad47ff7a98c507526acd93207c6ae86de483
Peter Strömberg <pez@pezius.com>
2022-05-14 22:14:58 +0200

    Fix bug with not derefing app-db

--- src/main/joyride/config.cljs
+++ src/main/joyride/config.cljs
@@ -5,6 +5,7 @@
 
 (def user-config-path (path/join (os/homedir) ".config"))
 (def user-scripts-path (path/join "joyride" "scripts"))
+
 (defn user-abs-scripts-path 
   "Returns the absolute path to the User scripts directory
    It's a function because `workspace-abs-scripts-path` is."
@@ -12,9 +13,10 @@
   (path/join user-config-path user-scripts-path))
 
 (def workspace-scripts-path (path/join ".joyride" "scripts"))
+
 (defn workspace-abs-scripts-path
   "Returns the absolute path to the Workspace scripts directory
    returns `nil` if there is not workspace root"
   []
-  (when-let [workspace-root (:workspace-root-path db/!app-db)]
+  (when-let [workspace-root (:workspace-root-path @db/!app-db)]
     (path/join workspace-root workspace-scripts-path)))
