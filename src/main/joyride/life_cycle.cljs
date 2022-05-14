5d9fbae94a7bc9c3b9ca8e7fa87d29db0bb06b6d
Peter Strömberg <pez@pezius.com>
2022-05-14 22:23:55 +0200

    Life-cycle init script needs to be a function

--- src/main/joyride/life_cycle.cljs
+++ src/main/joyride/life_cycle.cljs
@@ -17,14 +17,15 @@
    (path/join abs-scripts-path
               workspace-init-script)))
 
-(def init-scripts {:user {:label "User activate"
-                          :script user-init-script
-                          :script-path user-init-script-path
-                          :script-abs-path user-init-script-path}
-                   :workspace {:label "Workspace activate"
-                               :script user-init-script
-                               :script-path workspace-init-script-path
-                               :script-abs-path workspace-init-script-abs-path}})
+(defn init-scripts []
+  {:user {:label "User activate"
+          :script user-init-script
+          :script-path user-init-script-path
+          :script-abs-path user-init-script-path}
+   :workspace {:label "Workspace activate"
+               :script user-init-script
+               :script-path workspace-init-script-path
+               :script-abs-path (workspace-init-script-abs-path)}})
 
 (defn maybe-run-init-script+ [run-fn {:keys [label script script-path script-abs-path]}]
   (utils/say (str label " script: " script-path))
