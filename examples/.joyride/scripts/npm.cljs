d5c42ffbfa0ffa70e53675768097cca33cc2a55f
Michiel Borkent <michielborkent@gmail.com>
2022-10-11 17:09:41 +0200

    example

--- examples/.joyride/scripts/npm.cljs
+++ examples/.joyride/scripts/npm.cljs
@@ -2,3 +2,13 @@
   (:require ["moment" :as moment]))
 
 (.format (moment) "dddd")
+
+(require '["axios" :as axios]
+         '[promesa.core :as p])
+
+(def result (atom nil))
+(->
+ (axios.get "https://clojure.org")
+ (p/then #(reset! result %)))
+
+@result
