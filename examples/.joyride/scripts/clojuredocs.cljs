86204b08376b396026a32fa1214c87998f9c967f
Peter Strömberg <pez@pezius.com>
2022-10-29 01:42:02 +0200

    Make clojuredocs example use REPL first then fallback on clojure.core/<word>

--- examples/.joyride/scripts/clojuredocs.cljs
+++ examples/.joyride/scripts/clojuredocs.cljs
@@ -1,32 +1,46 @@
 (ns clojuredocs
-  (:require ["ext://betterthantomorrow.calva$v0" :refer [ranges]]
+  (:require ["ext://betterthantomorrow.calva$v0" :refer [ranges repl]]
             ["vscode" :as vscode]
             [clojure.string :as string]
-            [joyride.core :as joyride]))
+            [clojure.edn :as edn]
+            [joyride.core :as joyride]
+            [promesa.core :as p]))
+
+(defn resolve-in-repl [symbol-string]
+  (-> (p/let [result (repl.evaluateCode
+                      js/undefined
+                      (str "(some->> \"" symbol-string "\""
+                           "          symbol"
+                           "          resolve"
+                           "          symbol"
+                           "          str)"))
+              result-string (.-result result)
+              qualified-symbol-string (edn/read-string result-string)]
+        (or qualified-symbol-string
+            (str "clojure.core/" symbol-string)))
+      (p/catch #()) ; We fall back on clojure.core/<symbol-string>
+      (p/then (fn [s]
+                (if (empty? s)
+                  (str "clojure.core/" symbol-string)
+                  s)))))
 
 (defn clojuredocs-url [symbol-string]
-  (let [qualified-symbol-string (some->> symbol-string
-                                         symbol
-                                         resolve
-                                         symbol
-                                         str)]
-    (str "https://clojuredocs.org/"
-         ;; clean up ? ! &
-         (-> qualified-symbol-string
-             (string/replace "?" "%3f")
-             (string/replace "!" "%21")
-             (string/replace "&" "%26")))))
+  (-> (p/let [resolved (resolve-in-repl symbol-string)]
+        (str "https://clojuredocs.org/"
+             (-> resolved
+                 (string/replace "?" "%3f") ; clean up ? ! &
+                 (string/replace "!" "%21")
+                 (string/replace "&" "%26"))))))
 
 (defn lookup-current-form-or-selection []
-  (try (let [[_ lookup] (ranges.currentForm)
+  (-> (p/let [[_ lookup] (ranges.currentForm)
               url (clojuredocs-url lookup)]
-         (if url
-           (vscode/commands.executeCommand "simpleBrowser.show" url)
-           (vscode/window.showInformationMessage
-            (str "clojuredocs.cljs, can't resolve: " lookup))))
-      (catch :default e
-        (println (str "Clojuredocs lookup error: " e)))))
+        (if url
+          (vscode/commands.executeCommand "simpleBrowser.show" url)
+          (vscode/window.showInformationMessage
+           (str "clojuredocs.cljs, can't resolve: " lookup))))
+      (p/catch (fn [e]
+                 (println (str "Clojuredocs lookup error: " e))))))
 
 (when (= (joyride/invoked-script) joyride/*file*)
   (lookup-current-form-or-selection))
-
