a0acd098fe4732f7b914c013887d94b0f4a1dc84
Peter Strömberg <pez@pezius.com>
2023-01-01 17:27:00 +0100

    Cleanup
    Fixes #129

--- vscode-test-runner/workspace-1/.joyride/src/integration_test/npm_test.cljs
+++ vscode-test-runner/workspace-1/.joyride/src/integration_test/npm_test.cljs
@@ -2,11 +2,7 @@
   (:require [cljs.test :refer [deftest testing is]]
             ["color-convert" :as convert]))
 
-
 (deftest npm
   (testing "Can use required npm module"
     (is (= "7B2D43"
            (convert/rgb.hex 123 45 67)))))
-
-
-
