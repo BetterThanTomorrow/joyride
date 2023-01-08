ed09267a824d034a910ecdfe2e4c387fd6320995
Peter Strömberg <pez@pezius.com>
2023-01-08 16:13:51 +0100

    Add tests for requiring extension

---
+++ vscode-test-runner/workspace-1/.joyride/src/integration_test/require_extension_test.cljs
@@ -0,0 +1,18 @@
+(ns integration-test.require-extension-test
+  (:require [cljs.test :refer [deftest testing is]]
+            [promesa.core :as p]
+            ["ext://betterthantomorrow.joyride" :as ns-required-extension]
+            [integration-test.macros :refer [deftest-async]]))
+
+#_{:clj-kondo/ignore [:duplicate-require]}
+(require '["ext://betterthantomorrow.joyride" :as top-level-required-extension])
+
+(deftest-async ns-require-extension
+  (testing "ns form did require the Joyride extension"
+    (p/let [answer (ns-required-extension/runCode "42")]
+      (is (= 42 answer)))))
+
+(deftest-async top-level-require-extension
+  (testing "Requires the Joyride extension"
+    (p/let [question (top-level-required-extension/runCode "42")]
+      (is (= 42 question)))))
