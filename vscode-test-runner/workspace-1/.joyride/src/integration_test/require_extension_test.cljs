0b15a3e0d32fa4771947ce0fb31b6554ee5bc891
Peter Strömberg <pez@pezius.com>
2023-01-08 18:08:31 +0100

    Clean away unused refer

--- vscode-test-runner/workspace-1/.joyride/src/integration_test/require_extension_test.cljs
+++ vscode-test-runner/workspace-1/.joyride/src/integration_test/require_extension_test.cljs
@@ -1,5 +1,5 @@
 (ns integration-test.require-extension-test
-  (:require [cljs.test :refer [deftest testing is]]
+  (:require [cljs.test :refer [testing is]]
             [promesa.core :as p]
             ["ext://betterthantomorrow.joyride" :as ns-required-extension]
             [integration-test.macros :refer [deftest-async]]))
