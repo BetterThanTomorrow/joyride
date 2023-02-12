8b2351ece55df53c816fe8f46845e39eb15c86a4
Peter Strömberg <pez@pezius.com>
2023-02-12 23:53:01 +0100

    Install a dummy VSIX when running FS tests

--- vscode-test-runner/launch.js
+++ vscode-test-runner/launch.js
@@ -1,13 +1,13 @@
-const cp = require('child_process');
-const path = require('path');
-const process = require('process');
-const os = require('os');
-const fs = require('fs');
+const cp = require("child_process");
+const path = require("path");
+const process = require("process");
+const os = require("os");
+const fs = require("fs");
 const {
   downloadAndUnzipVSCode,
-  resolveCliPathFromVSCodeExecutablePath,
+  resolveCliArgsFromVSCodeExecutablePath,
   runTests,
-} = require('@vscode/test-electron');
+} = require("@vscode/test-electron");
 
 function init() {
   return new Promise((resolve, reject) => {
@@ -35,56 +35,67 @@ function init() {
 
 async function main(joyrideVSIXPathOrLabel, testWorkspace) {
   try {
-    const extensionTestsPath = path.resolve(__dirname, 'runTests');
-    const vscodeExecutablePath = await downloadAndUnzipVSCode('insiders');
-    const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+    const extensionTestsPath = path.resolve(__dirname, "runTests");
+    const vscodeExecutablePath = await downloadAndUnzipVSCode("insiders");
+    const [cliPath, ...args] =
+      resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
 
     const launchArgs = [
       testWorkspace,
-      '--verbose',
-      '--disable-workspace-trust'
+      ...args,
+      "--disable-workspace-trust",
+      ...(joyrideVSIXPathOrLabel !== "extension-development"
+        ? ["--install-extension", joyrideVSIXPathOrLabel, "--force"]
+        : [
+            // Install dummy extension to avoid runTests bug with not attaching to the spawned vscode instance
+            "--install-extension",
+            path.resolve(__dirname, "dummy", "dummy-0.0.1.vsix"),
+            "--force",
+          ]),
+      "--verbose",
     ];
-    if (joyrideVSIXPathOrLabel !== 'extension-development') {
-      launchArgs.push('--install-extension', joyrideVSIXPathOrLabel, '--force');
-    }
+    console.log("launchArgs", launchArgs);
     cp.spawnSync(cliPath, launchArgs, {
-      encoding: 'utf-8',
-      stdio: 'inherit',
+      encoding: "utf-8",
+      stdio: "inherit",
     });
 
     const runOptions = {
       vscodeExecutablePath,
-      reuseMachineInstall: true,
       extensionTestsPath,
       launchArgs: [testWorkspace],
     };
-    if (joyrideVSIXPathOrLabel === 'extension-development') {
-      runOptions.extensionDevelopmentPath = path.resolve(__dirname, '..');
+    if (joyrideVSIXPathOrLabel === "extension-development") {
+      runOptions.extensionDevelopmentPath = path.resolve(__dirname, "..");
     }
     await runTests(runOptions)
       .then((_result) => {
-        console.info('Tests finished');
+        console.info("Tests finished");
       })
       .catch((err) => {
-        console.error('Tests finished:', err);
+        console.error("Tests finished:", err);
         process.exit(1);
       });
   } catch (err) {
-    console.error('Failed to run tests:', err);
+    console.error("Failed to run tests:", err);
     process.exit(1);
   }
 }
 
-const args = require('minimist')(process.argv.slice(2));
-const joyrideVSIX = args['joyride-vsix'] ? args['joyride-vsix'] : 'extension-development';
-const testWorkspace = args['test-workspace']
-  ? path.resolve(args['test-workspace'])
-  : path.resolve(__dirname, '..', 'vscode-test-runner', 'workspace-1');
-console.info(`Using:\n  Joyride: ${joyrideVSIX}\n  Test workspace: ${testWorkspace}`);
+const args = require("minimist")(process.argv.slice(2));
+const joyrideVSIX = args["joyride-vsix"]
+  ? args["joyride-vsix"]
+  : "extension-development";
+const testWorkspace = args["test-workspace"]
+  ? path.resolve(args["test-workspace"])
+  : path.resolve(__dirname, "..", "vscode-test-runner", "workspace-1");
+console.info(
+  `Using:\n  Joyride: ${joyrideVSIX}\n  Test workspace: ${testWorkspace}`
+);
 
 void init()
   .then(() => main(joyrideVSIX, testWorkspace))
   .catch((error) => {
-    console.error('Failed to initialize test running environment:', error);
+    console.error("Failed to initialize test running environment:", error);
     process.exit(1);
   });
