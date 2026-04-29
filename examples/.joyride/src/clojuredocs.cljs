69e01792b950ab9ad40f228e845f9562017786d4
PEZ <pez@pezius.com>
2026-04-29 17:18:23 +0200

    Async instead of promesa

--- examples/.joyride/src/clojuredocs.cljs
+++ examples/.joyride/src/clojuredocs.cljs
@@ -1,11 +1,9 @@
 (ns clojuredocs
-  (:require ["ext://betterthantomorrow.calva$v0" :refer [ranges repl]]
+  (:require ["ext://betterthantomorrow.calva$v1" :refer [ranges repl]]
             ["vscode" :as vscode]
             [clojure.string :as string]
             [clojure.edn :as edn]
-            [joyride.core :as joyride]
-            [promesa.core :as p]
-            [clojure.string :as s]))
+            [joyride.core :as joyride]))
 
 ;; To use this un-ignore the relevant part in `user_activate.cljs`
 ;; Then register a keyboard shortcut something like this:
@@ -15,41 +13,40 @@
 ;;         "key": "ctrl+alt+c d",
 ;;     },
 
-(defn resolve-in-repl [symbol-string]
-  (-> (p/let [result (repl.evaluateCode
-                      js/undefined
-                      (str "(some->> \"" symbol-string "\""
-                           "          symbol"
-                           "          resolve"
-                           "          symbol"
-                           "          str)"))
-              result-string (.-result result)
-              qualified-symbol-string (edn/read-string result-string)]
-        (or qualified-symbol-string
-            (str "clojure.core/" symbol-string)))
-      (p/catch #()) ; We fall back on clojure.core/<symbol-string>
-      (p/then (fn [s]
-                (if (empty? s)
-                  (str "clojure.core/" symbol-string)
-                  s)))))
+(defn ^:async resolve-in-repl [symbol-string]
+  (try
+    (let [result (await (repl.evaluateCode
+                         js/undefined
+                         (str "(some->> \"" symbol-string "\""
+                              "          symbol"
+                              "          resolve"
+                              "          symbol"
+                              "          str)")))
+          result-string (.-result result)
+          qualified-symbol-string (edn/read-string result-string)]
+      (or qualified-symbol-string
+          (str "clojure.core/" symbol-string)))
+    (catch :default _ ; We fall back on clojure.core/<symbol-string>
+      (str "clojure.core/" symbol-string))))
 
-(defn clojuredocs-url [symbol-string]
-  (-> (p/let [resolved (resolve-in-repl symbol-string)]
-        (str "https://clojuredocs.org/"
-             (-> resolved
-                 (string/replace "?" "%3f") ; clean up ? ! &
-                 (string/replace "!" "%21")
-                 (string/replace "&" "%26"))))))
+(defn ^:async clojuredocs-url [symbol-string]
+  (let [resolved (await (resolve-in-repl symbol-string))]
+    (str "https://clojuredocs.org/"
+         (-> resolved
+             (string/replace "?" "%3f") ; clean up ? ! &
+             (string/replace "!" "%21")
+             (string/replace "&" "%26")))))
 
-(defn lookup-current-form-or-selection []
-  (-> (p/let [[_ lookup] (ranges.currentForm)
-              url (clojuredocs-url lookup)]
-        (if url
-          (vscode/commands.executeCommand "simpleBrowser.show" url)
-          (vscode/window.showInformationMessage
-           (str "clojuredocs.cljs, can't resolve: " lookup))))
-      (p/catch (fn [e]
-                 (println (str "Clojuredocs lookup error: " e))))))
+(defn ^:async lookup-current-form-or-selection []
+  (try
+    (let [[_ lookup] (await (ranges.currentForm))
+          url (await (clojuredocs-url lookup))]
+      (if url
+        (vscode/commands.executeCommand "simpleBrowser.show" url)
+        (vscode/window.showInformationMessage
+         (str "clojuredocs.cljs, can't resolve: " lookup))))
+    (catch :default e
+      (println (str "Clojuredocs lookup error: " e)))))
 
 (when (= (joyride/invoked-script) joyride/*file*)
   (lookup-current-form-or-selection))
